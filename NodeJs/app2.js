6f8315ee142ffe731387c0a5e1d920785e9490ea
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2025-02-21 14:36:02 +0300

    fs

--- NodeJs/app2.js
+++ NodeJs/app2.js
@@ -1,34 +1,56 @@
 const fs = require("fs/promises");
 
-async function readFileContent(filePath) {
+// async function readFileContent(filePath) {
+//   try {
+//     const data = await fs.readFile(filePath, "utf-8");
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// readFileContent("./t.text");
+
+// async function writeFileContent(filePath, content) {
+//   try {
+//     await fs.writeFile(filePath, content, "utf-8");
+//     console.log("Write");
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// writeFileContent("./t.text", "mosaa");
+
+// async function appendFileContent(filePath, content) {
+//   try {
+//     await fs.appendFile(filePath, `\n${content}`, "utf-8");
+//     console.log("Content ....");
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// appendFileContent("./t.text", "bye bye node");
+
+// async function deleteFileContent(filePath) {
+//   try {
+//     await fs.unlink(filePath);
+//     console.log("Deleted");
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// deleteFileContent("./t.text");
+
+async function renameFileContent(filePath, newName) {
   try {
-    const data = await fs.readFile(filePath, "utf-8");
-    console.log(data);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-readFileContent("./t.text");
-
-async function writeFileContent(filePath, content) {
-  try {
-    await fs.writeFile(filePath, content, "utf-8");
-    console.log("Write");
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-writeFileContent("./t.text", "mosaa");
-
-async function appendFileContent(filePath, content) {
-  try {
-    await fs.appendFile(filePath, `\n${content}`, "utf-8");
+    await fs.rename(filePath, newName);
     console.log("Content ....");
   } catch (error) {
     console.log(error.message);
   }
 }
 
-appendFileContent("./t.text", "bye bye node");
+renameFileContent("./t.text", "name");
